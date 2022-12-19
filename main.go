869bb9aed0dbe9500e45d194648345a6d77ac094
fairuszilvan <57885752+fairuszilvan@users.noreply.github.com>
2022-12-19 17:14:30 +0700

    fix bug

--- main.go
+++ main.go
@@ -60,7 +60,7 @@ func main() {
 	router.GET("/laporan", middleware.Login, controllers.IndexLaporan)
 	router.POST("/laporan", middleware.Login, controllers.InsertLaporan)
 	router.PUT("/laporan/laporan-update/:laporanID", middleware.Login, controllers.UpdateLaporan)
-	router.DELETE("/users/laporan-delete/:laporanID", middleware.Login, controllers.DeleteLaporan)
+	router.DELETE("/laporan/laporan-delete/:laporanID", middleware.Login, controllers.DeleteLaporan)
 
 	router.GET("/view", middleware.Login, controllers.CreateView)
 	router.GET("/view/show", middleware.Login, controllers.ViewLaporan)
